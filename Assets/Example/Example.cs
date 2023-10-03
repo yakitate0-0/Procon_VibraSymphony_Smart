4321d68de90792a4ad4cb977ef9be6f15f8d3137
yakitate0-0 <kentaka012345@gmail.com>
2023-10-03 23:46:07 +0900

    yakitate

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respond : MonoBehaviour
+{
+    public void home()
+    {
+
+        if (SystemInfo.supportsVibration)
+        {
+            Handheld.Vibrate();
+        }
+
+    }
+}
